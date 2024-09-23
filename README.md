44da5a0f740a66a05e8dcf3abbad3a8b4e240a92
Obegarde <5763603+Obegarde@users.noreply.github.com>
2024-09-23 14:33:38 +0200

    Add basic readme

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # space-finder
+
+A python script to find spaces in paths on linux.
+Without a path specified it will start from the CWD.
+The output is printed to console so can be piped to a file if needed.
+
--- space-finder.py
+++ space-finder.py
@@ -2,7 +2,7 @@ import os
 import argparse
 
 parser = argparse.ArgumentParser(description='A program to locate filepaths with spaces in them. Starting with the current working directory')
-parser.add_argument('path',default= os.path.dirname(__file__),nargs='?', help = 'Specify the folder you want to start finding spaces from')
+parser.add_argument('path',default=os.getcwd() ,nargs='?', help = 'Specify the folder you want to start finding spaces from')
 
 args = parser.parse_args()
 
